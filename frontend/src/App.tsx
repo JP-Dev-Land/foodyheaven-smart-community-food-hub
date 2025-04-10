b4feef09602310dc1424e4c068c5151d2bc98447
John-Prabu-A <johnprabu0702@gmail.com>
2025-04-10 09:36:29 +0530

    feat: implementation of tailwindcss and routing functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <nav className="p-4 bg-gray-100">
+        <Link to="/" className="mr-4 hover:underline">Home</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+      </nav>
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
